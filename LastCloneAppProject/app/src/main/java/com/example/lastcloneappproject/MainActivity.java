b7b9ec0fa5c538c54c1368ff2a4e87d2ff874d88
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-06-26 16:35:21 +0900

    main화면

--- LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/MainActivity.java
+++ LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lastcloneappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        Window window =getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(view.getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
     }
 }
