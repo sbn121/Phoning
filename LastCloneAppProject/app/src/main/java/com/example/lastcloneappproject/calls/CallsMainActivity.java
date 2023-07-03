c9bc9298fcd112f4df0c777e9eae5959606971b0
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-03 19:05:23 +0900

    통화 알림부분 추가

--- LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/calls/CallsMainActivity.java
+++ LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/calls/CallsMainActivity.java
@@ -3,6 +3,7 @@ package com.example.lastcloneappproject.calls;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.lastcloneappproject.HideActionBar;
 import com.example.lastcloneappproject.R;
@@ -12,6 +13,8 @@ public class CallsMainActivity extends AppCompatActivity {
 
     ActivityCallsMainBinding binding;
 
+    int count = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,19 @@ public class CallsMainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         binding.imgvBack.setOnClickListener(v -> {
             finish();
+        });
+        binding.imgvBell.setOnClickListener(v -> {
+
+            if(count % 2 == 1) {
+                binding.imgvBell.setImageResource(R.drawable.calls_bell2);
+                Toast.makeText(this, "이제부터 통화 알림을 받지 않아요.", Toast.LENGTH_SHORT).show();
+                count++;
+            } else {
+                binding.imgvBell.setImageResource(R.drawable.calls_bell);
+                Toast.makeText(this, "이제부터 통화 알림을 받을 수 있어요.", Toast.LENGTH_SHORT).show();
+                count++;
+            }
+
         });
     }
 }
