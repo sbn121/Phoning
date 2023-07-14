e9e846beec6872078ae1a8e948bb7a4e479d1ce3
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-07-14 11:21:18 +0900

    call layout 편집, calendar 날짜 선택시 스크롤바 이동 및 날짜 오류 수정

--- LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/calendar/ScheduleDTO.java
+++ LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/calendar/ScheduleDTO.java
@@ -2,7 +2,7 @@ package com.example.lastcloneappproject.calendar;
 
 public class ScheduleDTO {
 
-    private int imgv_profile, imgv_movie;
+    private int imgv_profile;
     private String tv_writer, tv_title, tv_chat, tv_time;
 
     public int getImgv_profile() {
@@ -11,14 +11,6 @@ public class ScheduleDTO {
 
     public void setImgv_profile(int imgv_profile) {
         this.imgv_profile = imgv_profile;
-    }
-
-    public int getImgv_movie() {
-        return imgv_movie;
-    }
-
-    public void setImgv_movie(int imgv_movie) {
-        this.imgv_movie = imgv_movie;
     }
 
     public String getTv_writer() {
@@ -53,9 +45,8 @@ public class ScheduleDTO {
         this.tv_time = tv_time;
     }
 
-    public ScheduleDTO(int imgv_profile, int imgv_movie, String tv_writer, String tv_title, String tv_chat, String tv_time) {
+    public ScheduleDTO(int imgv_profile, String tv_writer, String tv_title, String tv_chat, String tv_time) {
         this.imgv_profile = imgv_profile;
-        this.imgv_movie = imgv_movie;
         this.tv_writer = tv_writer;
         this.tv_title = tv_title;
         this.tv_chat = tv_chat;
