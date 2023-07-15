3c3e1308e02af3cd0ef692f07e8eebfd195c5f7d
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-16 01:45:15 +0900

    메시지 메인 수정

--- LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/messages/MessageMainActivity.java
+++ LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/messages/MessageMainActivity.java
@@ -34,6 +34,7 @@ public class MessageMainActivity extends AppCompatActivity {
 
     ArrayList<MessageMainDTO> list = new ArrayList<>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,29 +44,36 @@ public class MessageMainActivity extends AppCompatActivity {
         binding.imgvBack.setOnClickListener(v -> {
             finish();
         });
+        databaseReference = FirebaseDatabase.getInstance().getReference("해린1");
+
+
+
+
+
         adapter = new MessageMainAdapter(this, getlist());
         binding.recv.setAdapter(adapter);
         binding.recv.setLayoutManager(new LinearLayoutManager(this));
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    String content = snapshot.child("해린1").child("text").getValue(String.class);
-                    list.add(new MessageMainDTO(R.drawable.haerin3, "해린1", content, currentTime));
-                }
-                adapter.notifyDataSetChanged(); // 어댑터에 변경을 알림
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // onCancelled 메서드 내부에서 에러 처리 로직 작성
-            }
-        });
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    String content = snapshot.child("해린1").child("text").getValue(String.class);
+//                    list.add(new MessageMainDTO(R.drawable.haerin3, "해린1", content, currentTime));
+//                }
+//                adapter.notifyDataSetChanged(); // 어댑터에 변경을 알림
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                // onCancelled 메서드 내부에서 에러 처리 로직 작성
+//            }
+//        });
     }
 
     public ArrayList<MessageMainDTO> getlist() {
-        DatabaseReference chatReference = databaseReference.child("message").child("chat").child("다니엘1");
+        DatabaseReference chatReference = databaseReference.child("chat").child("다니엘1");
+        list.add(new MessageMainDTO(R.drawable.haerin3, "해린1", chatReference.getKey(), currentTime));
         list.add(new MessageMainDTO(R.drawable.minji3, "민지1", chatReference.getKey(), currentTime));
         list.add(new MessageMainDTO(R.drawable.danielle5, "다니엘1", chatReference.getKey(), currentTime));
         list.add(new MessageMainDTO(R.drawable.hanni11, "하니1", chatReference.getKey(), currentTime));
