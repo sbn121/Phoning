9659d54ac9b9f3ad1a8d6945ab2b94b9e20625c5
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-16 01:28:55 +0900

    메시지 메인 보이는부분 기능 수정

--- LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/messages/MessageMainActivity.java
+++ LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/messages/MessageMainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.lastcloneappproject.HideActionBar;
 import com.example.lastcloneappproject.R;
@@ -31,6 +32,8 @@ public class MessageMainActivity extends AppCompatActivity {
     SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
     String currentTime = dateFormat.format(new Date());
 
+    ArrayList<MessageMainDTO> list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,23 +43,34 @@ public class MessageMainActivity extends AppCompatActivity {
         binding.imgvBack.setOnClickListener(v -> {
             finish();
         });
-
         adapter = new MessageMainAdapter(this, getlist());
         binding.recv.setAdapter(adapter);
         binding.recv.setLayoutManager(new LinearLayoutManager(this));
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String content = snapshot.child("해린1").child("text").getValue(String.class);
+                    list.add(new MessageMainDTO(R.drawable.haerin3, "해린1", content, currentTime));
+                }
+                adapter.notifyDataSetChanged(); // 어댑터에 변경을 알림
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // onCancelled 메서드 내부에서 에러 처리 로직 작성
+            }
+        });
     }
 
     public ArrayList<MessageMainDTO> getlist() {
-        DatabaseReference chatReference = databaseReference.child("chat").child(MessageChatActivity.messageId);
-        ArrayList<MessageMainDTO> list = new ArrayList<>();
-        list.add(new MessageMainDTO(R.drawable.haerin3, "해린1", chatReference.getKey(), currentTime));
+        DatabaseReference chatReference = databaseReference.child("message").child("chat").child("다니엘1");
         list.add(new MessageMainDTO(R.drawable.minji3, "민지1", chatReference.getKey(), currentTime));
         list.add(new MessageMainDTO(R.drawable.danielle5, "다니엘1", chatReference.getKey(), currentTime));
-        list.add(new MessageMainDTO(R.drawable.hyein2, "혜인1", chatReference.getKey(), currentTime));
         list.add(new MessageMainDTO(R.drawable.hanni11, "하니1", chatReference.getKey(), currentTime));
+        list.add(new MessageMainDTO(R.drawable.hyein2, "혜인1", chatReference.getKey(), currentTime));
         list.add(new MessageMainDTO(R.drawable.newjeans11, "NewJeans1", chatReference.getKey(), currentTime));
         return list;
     }
-
-
 }
